e5d4cf50fe019f4eea11d3e1f9e873d2ee776ff0
Cem Yücehünkar <pd.cemyucehunkar@gmail.com>
2023-09-17 23:15:20 +0300

    Sayı Sıralama Programı

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a, b, c;
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("1.Sayı:");
+        a = input.nextInt();
+
+        System.out.print("2.Sayı:");
+        b = input.nextInt();
+
+        System.out.print("3.Sayı");
+        c = input.nextInt();
+
+        if ((a > b) && (a > c)) {
+            if (b > c) {
+                System.out.print("a>b>c");
+            } else {
+                System.out.print("a>c>b");
+
+            }
+        }  else if ((b>a) && (b>c)){
+            if (a>c){
+                System.out.print("b>a>c");
+            } else {
+                System.out.print("b>c>a");
+            }
+        }else {
+            if (a>b){
+                System.out.print("c>a>b");
+            } else {
+                System.out.print("c>b>a");
+        }
+        }
+
+    }
+}
+
